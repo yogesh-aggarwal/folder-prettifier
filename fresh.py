b6d3a373be7fc2b500000b7afbd0974aece6ae0b
Yogesh Aggarwal <developeryogeshgit@gmail.com>
2020-02-08 15:51:32 +0530

    Implemented all functions except categorize

--- fresh.py
+++ fresh.py
@@ -1,5 +1,6 @@
 import PySimpleGUI as sg
 import os
+# from time import sleep
 import platform
 
 sg.theme("reddit")
@@ -11,11 +12,13 @@ class Tools:
     def __init__(self):
         self.location = os.getcwd()
         self.platform = platform.system()
+        self.files = None
     
     def getFolderName(self, location=""):
         if self.platform == "Linux":
             location = self.location.split("/") if not location else location.split("/")
-            return location[len(location)-1]
+            print(location)
+            return location[len(location)-1] if location[len(location)-1] else location[len(location)-2]
     
     def getItems(self, location=""):
         try:
@@ -27,36 +30,49 @@ class Tools:
     def status(self, status):
         win.Element("_status_").Update(status)
 
-    def capitalize(self):
-        print("capitalize")
-
-    def underscore(self):
-        print("underscore")
-    
     def specificWord(self):
-        word = values["_removeWord_"]
-        print(f"specific word {word}")
+        self.files = self.getItems(self.location)
+        for file in self.files:
+            os.rename(file, file.replace(values["_removeWord_"], ""))
+    
+    def underscore(self):
+        self.files = self.getItems(self.location)
+        for file in self.files:
+            os.rename(file, file.replace("_", " "))
+
+    def capitalize(self):
+        self.files = self.getItems(self.location)
+        self.status("capitalizing files")
+        for file in self.files:
+            os.rename(file, file.capitalize())
     
     def casing(self):
         print("casing")
     
     def spacing(self):
-        print("spacing")
+        self.files = self.getItems(self.location)
+        for file in self.files:
+            with open(file, "w+") as file:
+                file.write([file.read().replace(f"{x} ", x).replace(f" {x}", x).replace(x, f"{x} ") for x in [".", ",", ":", "!", "?"]])
     
     def categorize(self):
         print("categorize")
 
     def action(self, values):
         actions = (
-            (values["_opCapitalize_"], self.capitalize),
-            (values["_opUnderscore_"], self.underscore),
             (values["_opSpecificWord_"], self.specificWord),
-            (values["_opCasing_"], self.casing),
+            (values["_opUnderscore_"], self.underscore),
+            (values["_opCapitalize_"], self.capitalize),
+            # (values["_opCasing_"], self.casing),
             (values["_opSpacing_"], self.spacing),
             (values["_opCategorize_"], self.categorize)
         )
+        self.status("Starting...")
+        self.location = values["_location_"]
+        self.status("Getting files ready...")
+        os.chdir(self.location)
         [action[1]() for action in actions if action[0]]
-        self.status("hn")
+        self.status("Done!")
 
 
 class Window(Tools):
@@ -74,7 +90,7 @@ class Window(Tools):
             [sg.Checkbox("Capitalize names", key="_opCapitalize_")],
             [sg.Checkbox("Remove underscore in names", key="_opUnderscore_")],
             [sg.Checkbox("Remove specific word in names", key="_opSpecificWord_", change_submits=True), sg.Input(key="_removeWord_", disabled=True)],
-            [sg.Checkbox("Handle casing mistakes in text files", key="_opCasing_")],
+            # [sg.Checkbox("Handle casing mistakes in text files", key="_opCasing_")],
             [sg.Checkbox("Handle spacing mistakes in text files", key="_opSpacing_")],
             [sg.Checkbox("Categorize files in respective folders", key="_opCategorize_")],
 
@@ -85,8 +101,6 @@ class Window(Tools):
 
     def createWin(self):
         self.win = sg.Window("Folder Prettfier", self.layout, resizable=0)
-        global win
-        win = self.win
 
     def operate(self):
         global values
@@ -103,7 +117,8 @@ class Window(Tools):
                 self.win.Element("_removeWord_").Update(disabled=False) if values["_opSpecificWord_"] else self.win.Element("_removeWord_").Update(disabled=True)
             
             if event == "_action_":
-                print(win, self.win)
+                global win
+                win = self.win
                 self.action(values)
 
     def close(self):
