c1404b7f1ed00611d32ff6d4615e1bf70b00c48d
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-08-10 20:03:38 +0530

    Made window not resizable

--- Forms/Main.Designer.cs
+++ Forms/Main.Designer.cs
@@ -401,6 +401,7 @@
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.folderActionsGroup);
             this.Controls.Add(this.folderOptionsGroup);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Main";
