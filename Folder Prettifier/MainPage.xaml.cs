2f4a3d63edee6a18048941c8a4d985b6a7214996
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-09-05 09:02:27 +0530

    Migrated to UWP

---
+++ Folder Prettifier/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Folder_Prettifier.Tools;
+
+
+namespace Folder_Prettifier
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            contentFrame.Navigate(Type.GetType("Folder_Prettifier.Pages.Settings"));
+        }
+
+        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            Windows.UI.Xaml.Controls.NavigationViewItem selectedItemTag = (Windows.UI.Xaml.Controls.NavigationViewItem)args.SelectedItem;
+            if (args.IsSettingsSelected)
+            {
+                contentFrame.Navigate(Type.GetType("Folder_Prettifier.Pages.Settings"));
+            }
+            else
+            {
+                contentFrame.Navigate(Type.GetType($"Folder_Prettifier.Pages.{selectedItemTag.Tag}"));
+            }
+        }
+
+        private void Login(object sender, TappedRoutedEventArgs e)
+        {
+            Tools.Tools.Login();
+        }
+    }
+}
---
+++ Folder Prettifier/Tools/Tools.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace Folder_Prettifier.Tools
+{
+    class Tools
+    {
+        async static public void Login()
+        {
+            Console.WriteLine("Login the user!");
+            ContentDialog dialog = new ContentDialog
+            {
+                Title= "Successfully logged in",
+                Content="You have logged in succefully & can take benifits of the restricted services",
+                CloseButtonText="Got it"
+            };
+            await dialog.ShowAsync();
+        }
+    }
+}
